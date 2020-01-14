410286d76ca3c0d5f4f31028a61fc3accb204940
ngkuru <ngkuru@mit.edu>
2020-01-14 16:28:07 -0500

    Better target selection for miners

--- mainplayer/RobotPlayer.java
+++ mainplayer/RobotPlayer.java
@@ -8,14 +8,15 @@ public strictfp class RobotPlayer {
     static Random rand = new Random();
 
     static Direction[] directions = {
-            Direction.NORTH,
-            Direction.NORTHEAST,
             Direction.EAST,
-            Direction.SOUTHEAST,
-            Direction.SOUTH,
-            Direction.SOUTHWEST,
             Direction.WEST,
-            Direction.NORTHWEST};
+            Direction.SOUTH,
+            Direction.NORTH,
+            Direction.SOUTHEAST,
+            Direction.NORTHWEST,
+            Direction.NORTHEAST,
+            Direction.SOUTHWEST,
+    };
 
     static RobotType[] spawnedByMiner = {RobotType.REFINERY, RobotType.VAPORATOR, RobotType.DESIGN_SCHOOL,
             RobotType.FULFILLMENT_CENTER, RobotType.NET_GUN};
@@ -26,6 +27,11 @@ public strictfp class RobotPlayer {
     static MapLocation hqLoc;
     static MapLocation target;
     static ArrayList<MapLocation> soupLocations = new ArrayList<MapLocation>();
+    static ArrayList<MapLocation> locationsTowardsTarget = new ArrayList<MapLocation>();
+
+    // This will be a string of the robot's current task
+    static String[] goingToCodes = {"soup", "HQ", "beginning"};
+    static String goingTo;
 
     /**
      * run() is the method that is called when a robot is instantiated in the Battlecode world.
@@ -95,11 +101,33 @@ public strictfp class RobotPlayer {
     static void runHQ() throws GameActionException {
         // TODO: Send location info to blockchain
 
+        // HQ also keeps track of soup locations and informs newly spawned miners.
+        updateSoupLocations();
+
+        // Check if we see soup
+        MapLocation soupLoc = seeSoup(rc.getType().sensorRadiusSquared);
+        // If we see soup, update soupLocations
+        if (soupLoc != null && !soupLocations.contains(soupLoc)) {
+            soupLocations.add(soupLoc);
+            broadcastLocation(soupLocations.get(0), "soup", 1);
+        }
+        // TODO: Create a blacklist for soup locations that are unreachable
+        // Else if we know where soup is, set target to soup
+        if (soupLoc != null) {
+            changeTarget(soupLoc, "soup");
+        } else if (target == null && soupLocations.size() > 0) {
+            changeTarget(soupLocations.get(rand.nextInt(soupLocations.size())), "soup");
+        }
+
         // Create 10 miners in the beginning of the game
         if (numMiners < 10) {
             for (Direction dir : directions) {
                 if (tryBuild(RobotType.MINER, dir)) {
                     numMiners++;
+                    //If successful, broadcast any soup location if known
+                    if (soupLocations.size() > 0) {
+                        broadcastLocation(soupLocations.get(0), "soup", 1);
+                    }
                 }
             }
         }
@@ -111,58 +139,72 @@ public strictfp class RobotPlayer {
         // Get status update on soup locations from blockchain
         updateSoupLocations();
         // If on a tile with soup info but no soup, inform others
-        System.out.println(rc.getLocation());
-        System.out.println(soupLocations);
         if (soupLocations.contains(rc.getLocation()) && rc.senseSoup(rc.getLocation()) == 0){
             broadcastLocation(rc.getLocation(), "no soup", 2);
         }
 
-        // If target is no longer in soupLocations, disable target
-        // TODO: Else if can see target but no soup - disable but without clash and multibroadcasting
-        if (target != null && !soupLocations.contains(target)) {
-            target = null;
+        // In the beginning, send targets directly to where they have been going for a while if we don't already know a soup location
+        // Else if they have been going for too long, remove target
+        if (turnCount == 1 && soupLocations.size() == 0) {
+            Direction dir = rc.getLocation().directionTo(hqLoc).opposite();
+            changeTarget(rc.getLocation().add(dir).add(dir).add(dir).add(dir).add(dir), "beginning");
+        } else if (turnCount == 15 && goingTo == "beginning") {
+            changeTarget(null, null);
+        }
+
+        // As a general rule, if reached target, disable target
+        // Else if going to soup but target is no longer in soupLocations, disable target
+        // TODO: Else if can see target but no soup or water - disable but without clash and multibroadcasting
+        if (target != null && rc.getLocation() == target) {
+            changeTarget(null, null);
+        } else if (goingTo == "soup" && !soupLocations.contains(target)) {
+            changeTarget(null, null);
         }
 
         // Check if we see soup
-        // MapLocation soupLoc = seeSoup(5);
         MapLocation soupLoc = seeSoup(10);
         // If we see soup, set target to soup
         // TODO: Create a blacklist for soup locations that are unreachable
         // Else if we know where soup is, set target to soup
         if (soupLoc != null) {
-            target = soupLoc;
+            changeTarget(soupLoc, "soup");
         } else if (target == null && soupLocations.size() > 0) {
-            target = soupLocations.get(rand.nextInt(soupLocations.size()));
+            changeTarget(soupLocations.get(rand.nextInt(soupLocations.size())), "soup");
         }
 
         // Try to mine soup, if we are able to, reset target
         for (Direction dir : directions) {
             if (tryMine(dir)) {
-                target = null;
+                changeTarget(null, null);
                 // If soup location is unknown, share it with others
                 if (!soupLocations.contains(rc.getLocation().add(dir))) {
                     broadcastLocation(rc.getLocation().add(dir), "soup", 1);
                 }
             }
         }
-
-        // If inventory is full, set target to HQ
-        if (rc.getSoupCarrying() == RobotType.MINER.soupLimit) {
-            target = hqLoc;
-        }
-
         // Try to refine soup, reset if successful
         for (Direction dir : directions) {
             if (tryRefine(dir)) {
-                target = null;
+                changeTarget(null, null);
             }
         }
 
-        // If there is a target, try to go to the target, if fails or no target, move randomly
-        System.out.println("target " + target);
+        // If inventory is full and not already going to HQ, set target to HQ
+        if (rc.getSoupCarrying() == RobotType.MINER.soupLimit && goingTo != "HQ") {
+            changeTarget(hqLoc, "HQ");
+        }
+
+        // If there is a target, try to go to the target and add new location to the list, if fails or no target, move randomly
         if (target != null) {
             rc.setIndicatorLine(rc.getLocation(), target, 255, 255, 0);
-            goTo(target);
+            // TODO: If going to HQ but wall has already started, build a refinery
+            if (goTo(target)) {
+                locationsTowardsTarget.add(rc.getLocation());
+                //If walked towards the same square 3 times, cancel target
+                if (Collections.frequency(locationsTowardsTarget, rc.getLocation()) >= 3) {
+                    changeTarget(null, null);
+                }
+            }
         }
         goTo(randomDirection());
     }
@@ -214,6 +256,19 @@ public strictfp class RobotPlayer {
     }
 
     /**
+     * Changes target, clears target past path.
+     *
+     * @param newTarget
+     * @param newTask
+     */
+    static void changeTarget(MapLocation newTarget, String newTask) {
+        target = newTarget;
+        goingTo = newTask;
+        locationsTowardsTarget.clear();
+        locationsTowardsTarget.add(rc.getLocation());
+    }
+
+    /**
      * Try to go towards a direction. If can't go directly, try to go via a 45 or 90 degree angle.
      *
      * @param dir
@@ -242,6 +297,12 @@ public strictfp class RobotPlayer {
         return goTo(rc.getLocation().directionTo(destination));
     }
 
+    /**
+     * Try to move (any direction).
+     *
+     * @return boolean: if successful
+     * @throws GameActionException
+     */
     static boolean tryMove() throws GameActionException {
         for (Direction dir : directions)
             if (tryMove(dir))
@@ -250,21 +311,21 @@ public strictfp class RobotPlayer {
     }
 
     /**
-     * Attempts to move in a given direction.
+     * Attempts to move in a given direction if we can and not flooded.
      *
      * @param dir The intended direction of movement
      * @return true if a move was performed
      * @throws GameActionException
      */
     static boolean tryMove(Direction dir) throws GameActionException {
-        if (rc.isReady() && rc.canMove(dir)) {
+        if (rc.isReady() && rc.canMove(dir) && !rc.senseFlooding(rc.getLocation().add(dir))) {
             rc.move(dir);
             return true;
         } else return false;
     }
 
     /**
-     * Attempts to build a given robot in a given direction.
+     * Attempts to build a given robot in a given direction if not flooded.
      *
      * @param type The type of the robot to build
      * @param dir The intended direction of movement
@@ -272,7 +333,7 @@ public strictfp class RobotPlayer {
      * @throws GameActionException
      */
     static boolean tryBuild(RobotType type, Direction dir) throws GameActionException {
-        if (rc.isReady() && rc.canBuildRobot(type, dir)) {
+        if (rc.isReady() && rc.canBuildRobot(type, dir) && !rc.senseFlooding(rc.getLocation().add(dir))) {
             rc.buildRobot(type, dir);
             return true;
         } else return false;
@@ -404,7 +465,8 @@ public strictfp class RobotPlayer {
             }
 
             // TODO: When learned how to use maps, change 100, 200 to soup code
-            if (message[1] == 100) {
+            if (message[1] == 100 &&
+                    !soupLocations.contains(new MapLocation(message[2], message[3]))) {
                 soupLocations.add(new MapLocation(message[2], message[3]));
             } else if (message[1] == 200 &&
                     soupLocations.contains(new MapLocation(message[2], message[3]))) {
